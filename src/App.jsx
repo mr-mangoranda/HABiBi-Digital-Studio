58e098d1e22fca0e1430fbc50b009d8f4baa4db6
mr-mangoranda <habibmangoranda15@gmail.com>
2026-02-13 20:20:14 +0800

    fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./Components/Header/Header"
 import Nav from "./Components/Nav/Nav"
 import Service from "./Components/Service/Service"
 import Project from "./Components/Project/Project"
-import Testimonial from './Components/Testimonial/testimonial'
+import Testimonial from './Components/Testimonial/Testimonial'
 import Team from './Components/Team/Team'
 
 function App() {
