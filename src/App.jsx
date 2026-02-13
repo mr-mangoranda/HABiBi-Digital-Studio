71b3078891ba9c3d5cd85771cc29cc7722f5292a
mr-mangoranda <habibmangoranda15@gmail.com>
2026-02-13 12:48:40 +0800

    Project Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from "./Components/Header/Header"
 import Nav from "./Components/Nav/Nav"
 import Service from "./Components/Service/Service"
+import Project from "./Components/Project/Project"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Nav/>
       <Header/>
       <Service/>
+      <Project/>
     </>
   )
 }
---
+++ src/Components/Project/Project.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import projectCSS from "./../Project/Project.module.css"
+import star from "./../../assets/star.png"
+import project1 from "./../../assets/project01.jpg"
+import project2 from "./../../assets/project02.jpg"
+import project3 from "./../../assets/project03.jpg"
+import project4 from "./../../assets/project04.jpg"
+import project5 from "./../../assets/project05.jpg"
+
+
+function Project() {
+    return (
+        <div className={`${projectCSS.projectWrapper} section`}>
+            <h1 className="section_title">
+                Have A Wide Range of <br /> Creative Projects
+                <img src={star} alt="" />
+            </h1>
+
+            <div className={projectCSS.projectContainer}>
+                <div className={projectCSS.projectCard}>
+                    <img src={project1} alt="" />
+                    <h3>Project 1</h3>
+                    <button>View Now</button>
+                </div>
+                <div className={projectCSS.projectCard}>
+                    <img src={project2} alt="" />
+                    <h3>Project 2</h3>
+                    <button>View Now</button>
+                </div>
+                <div className={projectCSS.projectCard}>
+                    <img src={project3} alt="" />
+                    <h3>Project 3</h3>
+                    <button>View Now</button>
+                </div>
+                <div className={projectCSS.projectCard}>
+                    <img src={project4} alt="" />
+                    <h3>Project 4</h3>
+                    <button>View Now</button>
+                </div>
+                <div className={projectCSS.projectCard}>
+                    <img src={project5} alt="" />
+                    <h3>Project 5</h3>
+                    <button>View Now</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project
